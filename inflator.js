719010384df59f0e53a8565ab613a8b6d64ac9c0
Alejandro Baltra <abaltra@gmail.com>
2015-05-15 15:21:33 -0300

    Changed from unrar to 7zip-rar for rar files. Updated travis conf

--- inflator.js
+++ inflator.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var uuid = require('node-uuid');
-var unrar = require('unrar');
 var tarball = require('tarball-extract');
 var async = require('async');
 var Q = require('q');
@@ -12,7 +11,7 @@ function extract7Zip (path, outpath, create_random_path, q) {
         if (exists) {
             outpath = create_random_path? outpath + uuid.v4() + '/' : outpath;
             var expander = new p7zip();
-            expander.test(path, {p: 'NONE'})
+            expander.test(path, {p: ''})
                 .then(function () {
                     expander.extractFull(path, outpath)
                         .then(function () {
@@ -84,86 +83,6 @@ function extractGZ (path, outpath, create_random_path, q) {
     });
 };
 
-function extractRar (path, outpath, create_random_path, q) {
-    fs.exists(outpath, function (exists) {
-        if (exists) {
-            create_random_path = create_random_path ? true : false;
-            var uid = uuid.v4();
-            outpath = create_random_path? outpath + uid + '/' : outpath;
-            var saved_routes = {};
-            var dirs = [];
-            var files = [];
-            var archive = null;
-            try {
-                archive = new unrar({
-                    path: path,
-                    arguments: ['-pNONE'],
-                    failOnPasswords: true
-                });
-            
-                if (create_random_path) {
-                    fs.mkdirSync(outpath);
-                }
-                archive.list(function (err, entries) {
-                    if (err) {
-                        return q.reject(err);
-                    }
-                    entries.forEach(function (entry) {
-                        if (entry.type === 'Directory') {
-                            var path_pieces = entry.name.split('/');
-                            var saved_pieces = path_pieces[0];
-                            saved_routes[saved_pieces] = true;
-                            dirs.push(saved_pieces);
-                            for (var i = 1; i < path_pieces.length; i++) {
-                                saved_pieces += ('/' + path_pieces[i]);
-                                if (saved_routes[saved_pieces]) continue;
-                                else {
-                                    saved_routes[saved_pieces] = true;
-                                    dirs.push(saved_pieces);
-                                }
-                            }
-                        } else {
-                            files.push(entry);
-                        }
-                    });
-                    dirs.sort(function (a, b) {
-                        return a.split('/').length - b.split('/').length;
-                    });
-
-                    async.eachSeries(dirs, function (direntry, cb) {
-                        fs.mkdir(outpath + direntry, function (err) {
-                            cb(err);
-                        });
-                    }, function (err) {
-                        if (err) {
-                            return q.reject(err);
-                        }
-                        async.each(files, function (fileentry, cb) {
-                            var stream = archive.stream(fileentry.name);
-                            try {
-                                stream.pipe(fs.createWriteStream(outpath + fileentry.name))
-                                cb();
-                            } catch (err) {
-                                cb(err);
-                            }
-                        }, function (err) {
-                            if (err) {
-                                q.reject(err);
-                            } else {
-                                q.resolve(outpath);
-                            }
-                        });
-                    });
-                });
-            } catch (err) {
-                return q.reject(err);
-            }
-        } else {
-            q.reject('Output path doesn\'t exist');
-        }
-    });
-};
-
 exports.unpackFile = function (path, outpath, create_random_path) {
     var deferred = Q.defer();
     if (!outpath) {
@@ -188,11 +107,7 @@ exports.unpackFile = function (path, outpath, create_random_path) {
                         extractTarGZ(path, outpath, create_random_path, deferred);
                     } else if (/(gz)$/i.test(path)) {
                         extractGZ(path, outpath, create_random_path, deferred);
-                    } else if (/(zip)$/i.test(path)) {
-                        extract7Zip(path, outpath, create_random_path, deferred);
-                    } else if (/(rar)$/i.test(path)) {
-                        extractRar(path, outpath, create_random_path, deferred);
-                    } else if (/(7z|zipx)$/i.test(path)) {
+                    } else if (/(zip|rar|7z|zipx)$/i.test(path)) {
                         extract7Zip(path, outpath, create_random_path, deferred);
                     } else {
                         return cb('File type not supported')
